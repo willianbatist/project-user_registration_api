88a041926531ce4fca97aa070ae67949c21c99e2
Willian <willian.alves.b15@gmail.com>
2022-11-17 18:31:24 -0300

    config: created user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  first_name: string;
+
+  @Column('text')
+  last_name: string;
+
+  @Column()
+  username: string;
+
+  @Column('text')
+  email: string;
+
+  @Column()
+  password: string;
+}
