a40a4d64e843389e91ca7ffceab66652a7a983cd
Willian <willian.alves.b15@gmail.com>
2022-11-17 18:50:15 -0300

    feat: created users.service

--- src/users/entities/user.providers.ts
+++ src/users/entities/user.providers.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { User } from './user.entity';
 
-export const photoProviders = [
+export const userProviders = [
   {
     provide: 'PHOTO_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
