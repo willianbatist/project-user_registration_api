c35e5c635b41e2b897ce9e260f3cb0c44bc067c2
Willian <willian.alves.b15@gmail.com>
2022-11-21 16:01:15 -0300

    fix: type of create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -27,7 +27,7 @@ export class UserController {
   }
 
   @Post('/user')
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<unknown> {
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<Users[]> {
     return this.userService.create(createUserDto);
   }
 
