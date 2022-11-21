84e4d8582e0be12c482545df5c4a68d0e3a60119
Willian <willian.alves.b15@gmail.com>
2022-11-20 21:03:48 -0300

    feat: created deleteUserById

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Users } from './entities/user.entity';
 import { UserService } from './users.service';
@@ -15,5 +23,10 @@ export class UserController {
   @Post('/user')
   async createUser(@Body() createUserDto: CreateUserDto): Promise<unknown> {
     return this.userService.create(createUserDto);
+  }
+
+  @Delete('/user/:id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
   }
 }
