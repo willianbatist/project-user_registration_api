b74c8bdbbb117946a2148889e21a346752da0a2a
Willian <willian.alves.b15@gmail.com>
2022-11-22 20:56:03 -0300

    feat: created validationBodyCreateUser

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -1,16 +1,20 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+export const validationBodyCreateUser = (body) => {
+  const { first_name, last_name, username, email, password } = body;
+  if (!first_name || !last_name || !username || !email || !password) {
+    throw new HttpException('some field is not filled', HttpStatus.BAD_REQUEST);
+  }
+};
+
 export const validationCreateUser = async (username, email, userRepository) => {
   const userName = await userRepository.findOne({ where: { username } });
   const userEmail = await userRepository.findOne({ where: { email } });
   if (userName) {
-    throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    throw new HttpException('User already exists', HttpStatus.CONFLICT);
   }
   if (userEmail) {
-    throw new HttpException(
-      'Email is already being used',
-      HttpStatus.BAD_REQUEST,
-    );
+    throw new HttpException('Email is already being used', HttpStatus.CONFLICT);
   }
   if (!userName && !userEmail) {
     return false;
