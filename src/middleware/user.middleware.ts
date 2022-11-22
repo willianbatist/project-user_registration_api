130161eb3f0d194996b0711e588c66729dbc5511
Willian <willian.alves.b15@gmail.com>
2022-11-22 11:11:02 -0300

    fix: validationCreateUser

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -4,10 +4,13 @@ export const validationCreateUser = async (username, email, userRepository) => {
   const userName = await userRepository.findOne({ where: { username } });
   const userEmail = await userRepository.findOne({ where: { email } });
   if (userName) {
-    throw new HttpException('User already exists', HttpStatus.OK);
+    throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
   }
   if (userEmail) {
-    throw new HttpException('Email is already being used', HttpStatus.OK);
+    throw new HttpException(
+      'Email is already being used',
+      HttpStatus.BAD_REQUEST,
+    );
   }
   if (!userName && !userEmail) {
     return false;
