77531758cac14a7bddfafbd03cb07102d28819b7
Willian <willian.alves.b15@gmail.com>
2022-11-21 21:27:13 -0300

    fix: listen of 3000 to 3010

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('http://localhost:3000/users');
+  await app.listen(3010);
+  console.log('http://localhost:3010/users');
 }
 bootstrap();
