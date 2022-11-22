a3e131fa8ed601f3ee8c59ad23413c7e2f01fafb
Willian <willian.alves.b15@gmail.com>
2022-11-22 15:44:06 -0300

    feat: add description in create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,32 @@
 export class CreateUserDto {
+  /**
+   * O primeiro nome será utilizado para qualquer coisa (Perfil, Home Page, etc) que precise exibir
+   * informações da pessoa conectada.
+   * @example "Willian"
+   */
   first_name: string;
+
+  /**
+   * O sobrenome será utilizado caso precise exibir ou utilizar o nome completo do usuário.
+   * @example "Alves Batista"
+   */
   last_name: string;
+
+  /**
+   * O username será utilizado para identificação unica do usuário.
+   * @example "willianbastist"
+   */
   username: string;
+
+  /**
+   * O email será utilizado para realizar login, cadastro e recebimentos de informativos.
+   * @example "example@email.com"
+   */
   email: string;
+
+  /**
+   * Senha criado pelo usuário para ter acesso ao sistema. A mesma será criptrografada.
+   * @example "password123"
+   */
   password: string;
 }
