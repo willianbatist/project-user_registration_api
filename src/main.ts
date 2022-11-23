9d857f03bd86c15b80a27587830ad99657e0155f
Willian <willian.alves.b15@gmail.com>
2022-11-23 15:18:26 -0300

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { apiDescription } from './utils/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('API de cadastro de usuários')
-    .setDescription('The Users API description')
+    .setDescription(apiDescription)
     .setVersion('1.0')
     .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors();
   await app.listen(3010);
   console.log('http://localhost:3010/api/');
 }
