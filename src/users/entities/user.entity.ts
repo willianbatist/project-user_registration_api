c30d90807f9883e426fdf3b653f55cd6c6ad1628
Willian Batista <willian.alves.b15@gmail.com>
2024-04-19 16:42:49 -0300

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,14 +8,7 @@ export class Users {
   @Column({ length: 50 })
   first_name: string;
 
-  @Column({ length: 50 })
-  last_name: string;
 
-  @Column({ length: 50 })
-  username: string;
-
-  @Column({ length: 50 })
-  email: string;
 
   @Column('text')
   password: string;
