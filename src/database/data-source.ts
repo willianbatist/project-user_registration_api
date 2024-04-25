e161e2f9423d17108d0d10675ee27527c6fe2558
Willian Batista <willian.alves.b15@gmail.com>
2024-04-25 20:44:33 -0300

    Update data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -9,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   database: 'db_users',
   entities: ['dist/**/*.entity{.js,.js}'],
   migrations: ['dist/users/migrations/*.js'],
-  synchronize: false,
+  synchronize: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
