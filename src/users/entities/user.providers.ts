191dcdbe58a70047a9ffef071e4525591858c159
Willian Batista <willian.alves.b15@gmail.com>
2024-04-29 15:34:05 -0300

    Update user.providers.ts

--- src/users/entities/user.providers.ts
+++ src/users/entities/user.providers.ts
@@ -5,6 +5,6 @@ export const userProviders = [
   {
     provide: 'USERS_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Users),
-
+    inject: ['DATA_SOURCE'],
   },
 ];
